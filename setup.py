b9f7c1bd048004e341812b42fea187f1be52163c
Sparta142 <spartamercenary@gmail.com>
2018-01-23 06:41:29 -0500

    Attempt to support a wider range of Python versions.

--- setup.py
+++ setup.py
@@ -43,7 +43,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet'
     ]
 )
