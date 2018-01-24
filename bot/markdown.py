fe226f4148b54d0fa884caa90c11ad277a4849e8
Sparta142 <spartamercenary@gmail.com>
2018-01-23 21:23:25 -0500

    Add capability to format SearchResults and Reddit comments

--- bot/markdown.py
+++ bot/markdown.py
@@ -1,3 +1,18 @@
+SEARCH_RESULT_FORMAT = '{title} | {author} | {downloads:,d} | {last_updated}'
+COMMENT_FORMAT = '''#LinkMod:
+
+Title | Author | Downloads | Last updated
+:--|:--|:--|:--
+{0}
+
+*****
+
+^Bleep ^bloop, ^I'm ^a ^bot ^that ^links ^Factorio ^mods. ^| \
+^Usage: ^(`linkmods: Squeak Through`) ^| \
+^[GitHub](https://github.com/Sparta142/factorio-mod-portal-bot)
+'''
+
+
 def hyperlink(text, url):
     """
     Format a hyperlink with the specified text,
@@ -11,3 +26,29 @@ def hyperlink(text, url):
     escaped_url = url.replace('(', r'\(').replace(')', r'\)')
 
     return '[{}]({})'.format(escaped_text, escaped_url)
+
+
+def format_search_result(result):
+    """
+    Format a single SearchResult in Markdown.
+
+    :param result: the search result to format
+    :return: the formatted result
+    """
+    return SEARCH_RESULT_FORMAT.format(
+        title=hyperlink(result.title, result.link),
+        author=hyperlink(result.author, result.author_link),
+        downloads=result.downloads,
+        last_updated=result.last_updated
+    )
+
+
+def format_comment(search_results):
+    """
+    Format a list of SearchResults in Markdown as a Reddit comment.
+
+    :param search_results: the search results to format with
+    :return: the formatted comment
+    """
+    results_str = '\n'.join(format_search_result(r) for r in search_results)
+    return COMMENT_FORMAT.format(results_str)
--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -1,4 +1,7 @@
-from bot.markdown import hyperlink
+from pytest import fixture
+
+from bot.markdown import hyperlink, format_search_result, format_comment
+from bot.portal import SearchResult
 
 
 class TestHyperlinkFunction(object):
@@ -31,3 +34,38 @@ class TestHyperlinkFunction(object):
         actual = hyperlink('text', 'http://example.com/Some_(thing)')
 
         assert expected == actual
+
+
+class TestFormatFunctions(object):
+    @fixture
+    def result(self):
+        return SearchResult(
+            title='SampleMod',
+            link='http://example.com/mod/SamplePageMod',
+            author='SampleAuthor',
+            author_link='http://example.com/user/SamplePageAuthor',
+            downloads=123456,
+            last_updated='a month ago'
+        )
+
+    def test_format_search_result_basic(self, result):
+        formatted = format_search_result(result)
+
+        # Just check that it contains all relevant data /somehow/
+        assert 'SampleMod' in formatted
+        assert 'http://example.com/mod/SamplePageMod' in formatted
+        assert 'SampleAuthor' in formatted
+        assert 'http://example.com/user/SamplePageAuthor' in formatted
+        assert '123,456' in formatted
+        assert 'a month ago' in formatted
+
+    def test_format_comment_basic(self, result):
+        formatted = format_comment([result] * 7)
+
+        # Just check that it contains all relevant data /somehow/
+        assert formatted.count('SampleMod') == 7
+        assert formatted.count('http://example.com/mod/SamplePageMod') == 7
+        assert formatted.count('SampleAuthor') == 7
+        assert formatted.count('http://example.com/user/SamplePageAuthor') == 7
+        assert formatted.count('123,456') == 7
+        assert formatted.count('a month ago') == 7
